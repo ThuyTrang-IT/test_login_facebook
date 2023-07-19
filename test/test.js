6d25647640da5cb2fedb74aaa97b45c818932255
ThuyTrang-IT <63693331+ThuyTrang-IT@users.noreply.github.com>
2023-07-19 17:46:55 +0700

    modified:   test/test.js

--- test/test.js
+++ test/test.js
@@ -1,45 +1,62 @@
 const puppeteer = require("puppeteer");
-const fs = require('fs');
+const fs = require("fs");
 
 (async () => {
-  const accountsFilePath = 'fb account.txt';
-  const accounts = fs.readFileSync(accountsFilePath, 'utf-8').split('\n');
+  // Đọc nội dung của file "fb account.txt"
+  const fileContent = fs.readFileSync("fb account.txt", "utf-8");
+  const accounts = fileContent.split("\n");
 
-  const browser = await puppeteer.launch({ headless: false }); // Đặt headless thành true để không hiển thị trình duyệt
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.setViewport({ width: 1366, height: 768 });
 
   for (const account of accounts) {
-    const userData = parseAccountData(account);
-    if (userData) {
-      const loginResult = await testLogin(page, userData);
-      console.log(`${userData.email}: ${loginResult}`);
+    const [uid, pass, fa, cookie, email, passmail] = account
+      .split("|")
+      .map((item) => item.trim());
+
+    // Navigate to Facebook login page
+    await page.goto("https://www.facebook.com/");
+
+    // Fill in login form
+    await page.type("#email", email || uid);
+    await page.type("#pass", pass);
+
+    // Submit login form
+    await page.click("button[type='submit']");
+
+    // Wait for navigation to complete
+    await page.waitForNavigation();
+
+    // Check if 2FA is required
+    if (await page.$("#approvals_code")) {
+      if (!fa) {
+        console.log(`2FA code is required for account with UID: ${uid}`);
+        continue;
+      }
+
+      // Fill in 2FA code
+      await page.type("#approvals_code", fa);
+      await page.click("#checkpointSubmitButton");
+
+      // Wait for navigation to complete
+      await page.waitForNavigation();
     }
+
+    // Check if login was successful
+    if (page.url().startsWith("https://www.facebook.com/")) {
+      console.log(`Login successful for account with UID: ${uid}`);
+    } else {
+      console.log(`Login failed for account with UID: ${uid}`);
+    }
+
+    // Clear cookies for the next login attempt
+    await page.deleteCookie();
+
+    // Wait a short time before logging in with the next account
+    await page.waitForTimeout(2000);
   }
 
+  // Close the browser
   await browser.close();
 })();
-
-function parseAccountData(account) {
-  const [email, pass] = account.split(':');
-  if (email && pass) {
-    return { email: email.trim(), pass: pass.trim() };
-  }
-  return null;
-}
-
-async function testLogin(page, userData) {
-  await page.goto("https://www.facebook.com/");
-
-  await page.type("#email", userData.email);
-  await page.type("#pass", userData.pass);
-  await page.click("button[type='submit']");
-
-  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
-
-  if (page.url().startsWith("https://www.facebook.com/login")) {
-    return "Login failed";
-  } else {
-    return "Login successful";
-  }
-}
