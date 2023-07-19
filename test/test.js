55d648fc99ed38852c212aa5de10f26560ac918f
ThuyTrang-IT <63693331+ThuyTrang-IT@users.noreply.github.com>
2023-07-19 17:25:49 +0700

    new file:   fb account.txt 	modified:   test/test.js

--- test/test.js
+++ test/test.js
@@ -1,57 +1,45 @@
 const puppeteer = require("puppeteer");
+const fs = require('fs');
 
 (async () => {
-  const userData = {
-    uid: "100093953724492",
-    pass: "Nert67cfQK5",
-    fa: "4CM2AFEEAHZQNWSFVD2CC4Q5KQBMI3VG",
-    cookie:
-      "sb=Y6eiZGBmWiFjFr_8AZU1-_ak;fr=02mMkxU3ahamN6GGt.AWXhqEb0sNj1PP1HPcg5Ga1F2R8.BkomGp..AAA.0.0.BkomGp.AWU;datr=qWGiZNfWuwd5ZteLTHZoctWT;xs=19:7stD9ffNfWLk6g:2:1688363433:;c_user=100093953724492;m_page_voice=100093953724492;wd=200x200;m_pixel_ratio=1;",
-    email: "rachkidjatiew@hotmail.com",
-    passmail: "V6WL68Rb46",
-  };
+  const accountsFilePath = 'fb account.txt';
+  const accounts = fs.readFileSync(accountsFilePath, 'utf-8').split('\n');
 
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: false }); // Đặt headless thành true để không hiển thị trình duyệt
   const page = await browser.newPage();
   await page.setViewport({ width: 1366, height: 768 });
 
-  // Set cookies for the user
-  /* const cookies = userData.cookie.split(";").map((pair) => {
-    const [name, value] = pair.trim().split("=");
-    return { name, value, domain: ".facebook.com" };
-  });
-  await page.setCookie(...cookies); */
-
-  // Navigate to Facebook login page
-  await page.goto("https://www.facebook.com/");
-
-  // Fill in login form
-  await page.type("#email", userData.email);
-  await page.type("#pass", userData.pass);
-
-  // Submit login form
-  await page.click("button[type='submit']");
-
-  // Wait for navigation to complete
-  await page.waitForNavigation();
-
-  // Check if 2FA is required
-  if (await page.$("#approvals_code")) {
-    // Fill in 2FA code
-    await page.type("#approvals_code", userData.fa);
-    await page.click("#checkpointSubmitButton");
-
-    // Wait for navigation to complete
-    await page.waitForNavigation();
+  for (const account of accounts) {
+    const userData = parseAccountData(account);
+    if (userData) {
+      const loginResult = await testLogin(page, userData);
+      console.log(`${userData.email}: ${loginResult}`);
+    }
   }
 
-  // Check if login was successful
-  if (page.url().startsWith("https://www.facebook.com/")) {
-    console.log("Login failed");
-  } else {
-    console.log("Login successful");
-  }
-
-  // Close the browser
   await browser.close();
 })();
+
+function parseAccountData(account) {
+  const [email, pass] = account.split(':');
+  if (email && pass) {
+    return { email: email.trim(), pass: pass.trim() };
+  }
+  return null;
+}
+
+async function testLogin(page, userData) {
+  await page.goto("https://www.facebook.com/");
+
+  await page.type("#email", userData.email);
+  await page.type("#pass", userData.pass);
+  await page.click("button[type='submit']");
+
+  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+
+  if (page.url().startsWith("https://www.facebook.com/login")) {
+    return "Login failed";
+  } else {
+    return "Login successful";
+  }
+}
